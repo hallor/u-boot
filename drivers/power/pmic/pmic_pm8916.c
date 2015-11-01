ef9dbcd8ab06ee044d8d304ed57d273b4769e57e
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-11-01 10:53:20 +0100

    pmic: add stub driver for pm8916
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>

---
+++ drivers/power/pmic/pmic_pm8916.c
@@ -0,0 +1,17 @@
+/*
+ * Qualcomm pm8916 pmic driver
+ *
+ * (C) Copyright 2015 Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+// TODO
+//from LK
+// soc connected via i2c-like bus
+//spmi v2
+
+int power_pm8916_init(void)
+{
+	return 0;
+}
