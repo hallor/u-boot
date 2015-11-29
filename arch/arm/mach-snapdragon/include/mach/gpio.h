42ab9fc116842609dae4c4aec9fe5f7a3cdf6784
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-11-29 19:00:25 +0100

    arm: Add Qualcomm Snapdragon machine architecture
    Add support for Qualcomm Snapdragon family of SoCs.
    First SoC supported is APQ8016 / MSM8916.
    
    Devices included in mach code:
    - Simple clock configuration functions
    - SPMI (pmic bus) driver
    - Reset controller
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>

---
+++ arch/arm/mach-snapdragon/include/mach/gpio.h
@@ -0,0 +1,25 @@
+/*
+ * Qualcomm GPIO definitions
+ *
+ * (C) Copyright 2015 Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef _ASM_ARCH_GPIO_H
+#define _ASM_ARCH_GPIO_H
+
+/* Register offsets */
+#define GPIO_CONFIG_OFF(no)         ((no)*0x1000)
+#define GPIO_IN_OUT_OFF(no)         ((no)*0x1000 + 0x4)
+
+/* OE */
+#define GPIO_OE_DISABLE	(0x0 << 9)
+#define GPIO_OE_ENABLE	(0x1 << 9)
+#define GPIO_OE_MASK	(0x1 << 9)
+
+/* GPIO_IN_OUT register shifts. */
+#define GPIO_IN         0
+#define GPIO_OUT        1
+
+#endif /* _ASM_ARCH_GPIO_H */
+
---
+++ arch/arm/mach-snapdragon/include/mach/sysmap.h
@@ -0,0 +1,11 @@
+#ifndef SYSMAP_H
+#define SYSMAP_H
+
+#define CLK_CTL_BASE     0x1800000
+#define MSM_SPMI_BASE    0x2000000LL
+
+#define GICD_BASE 0x0b000000
+#define GICC_BASE 0x0a20c000
+
+#endif /* SYSMAP_H */
+
