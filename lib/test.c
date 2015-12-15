a7082b2b627ad483d82775755b6e92ae86006b45
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-12-15 11:14:30 +0100

    test script add

---
+++ lib/test.c
@@ -0,0 +1,28 @@
+/*
+ *  linux/lib/string.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
+
+/*
+ * stupid library routines.. The optimized versions should generally be found
+ * as inline code in <asm-xx/string.h>
+ *
+ * These are buggy as well..
+ *
+ * * Fri Jun 25 1999, Ingo Oeser <ioe@informatik.tu-chemnitz.de>
+ * -  Added strsep() which will replace strtok() soon (because strsep() is
+ *    reentrant and should be faster). Use only strsep() in new code, please.
+ */
+
+#include <linux/types.h>
+#include <linux/string.h>
+#include <linux/ctype.h>
+#include <malloc.h>
+#include <console.h>
+
+
+int testtest(void)
+{
+printf("this is test\n");
+}
